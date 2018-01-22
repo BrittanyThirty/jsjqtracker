c110e3cd7072f6273a0008d9644db8c9361dcd98
BrittanyThirty <bmkthirty@gmail.com>
2018-01-21 20:58:26 -0800

    updates values in html begins changes to adjust for string in js, works on logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,40 +1,24 @@
-//
-//
-// var mostlyA = 5
-//    if (result < 16) {
-//      $('#value').show();
-//    } else if (result === 20) {
-//      $('#value').show();
-//    } else if (result === 15) {
-//      $('#value').show();
-//    } else if (result) === 10) {
-//      $('#value').show();
-//    } else if (result === 5) {
-//      $('#value').show();
-//    } else {
-//      $('#under-').show();
-//    }
-//
-//    var mostlyA = 5
-//    var mostlyB = 10
-//    var mostlyC = 15
-//    var mostlyD = 20
-//     if (mostlyA == 5) {
-//       alert("You selected mostly A's! You should look into the C# cohort here at Epicodus!")
-//     } else if (mostlyB == 10){
-//       alert("You selected mostly B's! You should look into the D# cohort here at Epicodus!")
-//     } else if (mostlyC == 15){
-//       alert("You selected mostly B's! You should look into the E# cohort here at Epicodus!")
-//     } else {
-//       alert("You selected mostly D's! You should look into the F# cohort here at Epicodus!")
-//     }
-
-
-
-//Back(Business)
-$document.ready(function())  {
-  $("#formOne").submit(function(event) {
+$(document).ready(function()) {
+  $(".form").submit(function(event) {
     event.preventDefault();
-
   }
     });
+    var mostly1 = parseInt($("preference1").val());
+    var mostly2 = 10
+    var mostly3 = 15
+    var mostly4 = 20
+    
+
+   if (result <= 16) {
+     $('#value').show();
+   } else if (result === 20) {
+     $('#value').show();
+   } else if (result === 15) {
+     $('#value').show();
+   } else if (result) === 10) {
+     $('#value').show();
+   } else if (result === 5) {
+     $('#value').show();
+   } else {
+     $('#under-').show();
+   }
