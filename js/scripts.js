4ecdaf86031d8b9282446f32d816e12993a67ab4
BrittanyThirty <bmkthirty@gmail.com>
2018-01-21 20:08:36 -0800

    adds headers in html, closes divs, updates id names, adds style tags

--- js/scripts.js
+++ js/scripts.js
@@ -1,55 +1,40 @@
-function toPath(ltr)  {
-var result= '';
-var userinput = ["a","b","c","d"]
-var suggestion = ["CSharp","PHP","Java", "You're a chamelion"]
- if (var = 0; i>userinput.length;i++) {
-
-var mostlyA = 5
-   if (result < 16) {
-     $('#value').show();
-   } else if (result === 20) {
-     $('#value').show();
-   } else if (result === 15) {
-     $('#value').show();
-   } else if (result) === 10) {
-     $('#value').show();
-   } else if (result === 5) {
-     $('#value').show();
-   } else {
-     $('#under-').show();
-   }
-
-   var mostlyA = 5
-   var mostlyB = 10
-   var mostlyC = 15
-   var mostlyD = 20
-    if (mostlyA == 5) {
-      alert("You selected mostly A's! You should look into the C# cohort here at Epicodus!")
-    } else if (mostlyB == 10){
-      alert("You selected mostly B's! You should look into the D# cohort here at Epicodus!")
-    } else if (mostlyC == 15){
-      alert("You selected mostly B's! You should look into the E# cohort here at Epicodus!")
-    } else {
-      alert("You selected mostly D's! You should look into the F# cohort here at Epicodus!")
-    }
+//
+//
+// var mostlyA = 5
+//    if (result < 16) {
+//      $('#value').show();
+//    } else if (result === 20) {
+//      $('#value').show();
+//    } else if (result === 15) {
+//      $('#value').show();
+//    } else if (result) === 10) {
+//      $('#value').show();
+//    } else if (result === 5) {
+//      $('#value').show();
+//    } else {
+//      $('#under-').show();
+//    }
+//
+//    var mostlyA = 5
+//    var mostlyB = 10
+//    var mostlyC = 15
+//    var mostlyD = 20
+//     if (mostlyA == 5) {
+//       alert("You selected mostly A's! You should look into the C# cohort here at Epicodus!")
+//     } else if (mostlyB == 10){
+//       alert("You selected mostly B's! You should look into the D# cohort here at Epicodus!")
+//     } else if (mostlyC == 15){
+//       alert("You selected mostly B's! You should look into the E# cohort here at Epicodus!")
+//     } else {
+//       alert("You selected mostly D's! You should look into the F# cohort here at Epicodus!")
+//     }
 
 
-
-   // assuming:
-   // a=1  5
-   // b=2  10
-   // c=3  15
-   // d=4  20
 
 //Back(Business)
 $document.ready(function())  {
-  $(".form").submit(function() {
-    alert("Your form has been submitted");
-    event.preventDefault();
-  }
-    });
-
-
-    $ (document).ready(function() {
   $("#formOne").submit(function(event) {
     event.preventDefault();
+
+  }
+    });
