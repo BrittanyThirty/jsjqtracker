084d8f3d550193654206fc3067ccad856891f727
BrittanyThirty <bmkthirty@gmail.com>
2018-01-21 19:54:42 -0800

    adds request for user contact information in html

--- js/scripts.js
+++ js/scripts.js
@@ -4,20 +4,37 @@ var userinput = ["a","b","c","d"]
 var suggestion = ["CSharp","PHP","Java", "You're a chamelion"]
  if (var = 0; i>userinput.length;i++) {
 
-
-   if (answer < 16) {
+var mostlyA = 5
+   if (result < 16) {
      $('#value').show();
-   } else if (answer === 20) {
+   } else if (result === 20) {
      $('#value').show();
-   } else if (answer === 15) {
+   } else if (result === 15) {
      $('#value').show();
-   } else if (answer === 10) {
+   } else if (result) === 10) {
      $('#value').show();
-   } else if (answer === 5) {
+   } else if (result === 5) {
      $('#value').show();
    } else {
      $('#under-').show();
    }
+
+   var mostlyA = 5
+   var mostlyB = 10
+   var mostlyC = 15
+   var mostlyD = 20
+    if (mostlyA == 5) {
+      alert("You selected mostly A's! You should look into the C# cohort here at Epicodus!")
+    } else if (mostlyB == 10){
+      alert("You selected mostly B's! You should look into the D# cohort here at Epicodus!")
+    } else if (mostlyC == 15){
+      alert("You selected mostly B's! You should look into the E# cohort here at Epicodus!")
+    } else {
+      alert("You selected mostly D's! You should look into the F# cohort here at Epicodus!")
+    }
+
+
+
    // assuming:
    // a=1  5
    // b=2  10
