514f94693bf523b3e018f943431c5571e49d11e5
BrittanyThirty <32760538+BrittanyThirty@users.noreply.github.com>
2018-01-22 16:40:54 -0800

    Corrects logic, adds missing punctuation.

--- js/scripts.js
+++ js/scripts.js
@@ -6,22 +6,22 @@ $(document).ready(function(){
     var mostly3 = parseInt($("#preference3").val());
     var mostly4 = parseInt($("#preference4").val());
     var mostly5 = parseInt($("#preference5").val());
-    var result = mostly1 + mostly2 + mostly3 + mostly4 + mostly5
+    var result = mostly1 + mostly2 + mostly3 + mostly4 + mostly5;
 
-   if ((20 <= result) && (16 >= result)) {
+   if ((20 == result) && (16 <= result)) {
      $(".csharp, .ruby, .php").hide();
      $(".java").show();
-   } else if ((15 <= result) && (13 >= result)) {
+   } else if ((15 >= result) && (13 <= result)) {
      $(".java, .ruby, .php").hide();
      $(".csharp").show();
-   } else if ((12 <= result) && (10 >= result)) {
+   } else if ((12 >= result) && (10 <= result)) {
      $(".csharp, .java, .php").hide();
      $(".ruby").show();
-   } else if ((9 <= result) && (5 >= result)) {
+   } else if ((9 >= result) && (5 <= result)) {
      $(".csharp, .ruby, .java").hide();
      $(".php").show();
    } else {
-    Alert ("Complete form inputs.")
+    alert ("Complete form inputs.");
    }
   });
 });
